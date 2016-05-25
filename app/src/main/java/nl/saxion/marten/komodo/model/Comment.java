9ad277ce6e9773b14555fe3dacf29b74dbf37cc6
fatahfattah <fatahfattah@hotmail.com>
2016-05-25 13:37:43 +0200

    Update comment and thread

--- app/src/main/java/nl/saxion/marten/komodo/model/Comment.java
+++ app/src/main/java/nl/saxion/marten/komodo/model/Comment.java
@@ -1,21 +1,27 @@
 package nl.saxion.marten.komodo.model;
 
-import java.util.Date;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Gebruiker on 5/18/2016. 
  */
 
 public class Comment {
-    private static int comment_id;
+    private int comment_id;
     private String text;
-    private String createdOn;
-    private User author; // Door wie 
-    private Thread inThread; // In welke thread 
-    private int totalKudos;
+    private String created_at;
+    private int user_id; // Door wie
+    private int thread_id; // In welke thread
+    private int total_kudos;
 
-    public Comment() {
-        comment_id = comment_id + 1; // comment id verhoogt met 1 iedere keer dat een comment object aangemaakt wordt 
-        createdOn = new Date().toString();
+    public Comment(JSONObject commentObject) throws JSONException{
+        this.comment_id = commentObject.getInt("comment_id");
+
+        this.text = commentObject.getString("text");
+        this.created_at = commentObject.getString("created_at");
+        this.user_id = commentObject.getInt("user_id");
+        this.thread_id = commentObject.getInt("thread_id");
+        this.total_kudos = commentObject.getInt("total_kudos");
     }
 } 
--- app/src/main/java/nl/saxion/marten/komodo/model/Thread.java
+++ app/src/main/java/nl/saxion/marten/komodo/model/Thread.java
@@ -1,6 +1,9 @@
 package nl.saxion.marten.komodo.model;
 
-import java.util.Date;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.List;
 
 /**
@@ -12,23 +15,32 @@ public class Thread {
 
     private String title;
     private String text;
-    private String short_description;
 
-    private String createdOn;
-    private User createdBy;
-    //private Comment lastComment;
+    private String created_at;
+    private int user_id;
     private int totalKudos;
-    //private int totalComments;
-    //private int totalViews;
+    private int totalComments;
+    private int totalViews;
     private int totalSubscribed;
 
     private List<Comment> comments;
-    private List<User> usersThatCommented; // lijst van users die een comment geplaats hebben 
 
-    public Thread() {
-        thread_id = thread_id + 1; // thread id verhoogt met 1 ieder keer dat een nieuwe thread object aangemaakt wordt 
-        createdOn = new Date().toString();
-        //lastComment = comments.get(comments.size() - 1);
+    public Thread(JSONObject threadobject) throws JSONException {
+        this.thread_id = threadobject.getInt("thread_id");
+
+        this.title = threadobject.getString("title");
+        this.text = threadobject.getString("text");
+        this.created_at = threadobject.getString("created_at");
+        this.user_id = threadobject.getInt("user_id");
+        this.totalKudos = threadobject.getInt("total_kudos");
+        this.totalComments = threadobject.getInt("total_comments");
+        this.totalViews = threadobject.getInt("total_views");
+        this.totalSubscribed = threadobject.getInt("total_subscribed");
+
+        JSONArray threadarray = threadobject.getJSONArray("comments");
+        for (int i = 0; i < threadarray.length() ; i++) {
+            comments.add(new Comment(threadarray.getJSONObject(i)));
+        }
     }
 
     public Comment getLastComment() { //waarom wil je dit?
